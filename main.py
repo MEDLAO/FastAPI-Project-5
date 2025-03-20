e4d9c764f7d97f0823dd3d60c5ca545d795abe5b
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-03-20 21:46:31 +0000

    Start Project

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import os
+
+
+app = FastAPI()
+
+
+RAPIDAPI_SECRET = os.getenv("RAPIDAPI_SECRET")
+
+
+@app.get("/")
+def read_root():
+    welcome_message = (
+        "Welcome!"
+        "¡Bienvenido!"
+        "欢迎!"
+        "नमस्ते!"
+        "مرحبًا!"
+        "Olá!"
+        "Здравствуйте!"
+        "Bonjour!"
+        "বাংলা!"
+        "こんにちは!"
+    )
+    return {"message": welcome_message}
+
